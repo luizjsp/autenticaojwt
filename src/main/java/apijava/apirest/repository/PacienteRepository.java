d8b079a2d1a9c94e6da10eb09c7ba22f43224728
luizjsp <95031013+luizjsp@users.noreply.github.com>
2022-11-27 17:08:34 -0300

    Delete PacienteRepository.java

--- src/main/java/apijava/apirest/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-//package apijava.apirest.repository;
-
-//import apijava.apirest.model.PacienteModel;
-//import org.springframework.data.repository.CrudRepository;
-
-//public interface PacienteRepository extends CrudRepository<PacienteModel, Integer> {
-//}
